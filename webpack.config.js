30f06452b38cb8521bf693f1c3110a58ede6a7cd
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-03-13 01:39:47 +0300

    init: init presentation

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          port: 9000,
+          static: {
+            directory: path.join(__dirname, 'public'),
+          },
+        },
+      };
+
+const esLintPlugin = (isDev) => (isDev ? [] : [new ESLintPlugin({ extensions: ['js'] })]);
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|mp3)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /.json$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+  ],
+  ...devServer(development),
+});
